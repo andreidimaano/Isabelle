f2a965d6d3ddb93e0eaf62ea9c3d9ee19f9dce18
Jarvis Bui <jarvisbui@jarvis-mbp.lan>
2020-11-22 21:05:28 -0800

    Fixes #5

---
+++ src/DiscordBot.ts
@@ -0,0 +1,32 @@
+import{ Client } from 'discord.js';
+
+export class DiscordBot{
+    private static instance: DiscordBot;
+
+    private client: Client = new Client();
+
+    private constructor(){
+        this.initializeClient();
+    }
+
+    connect(): void {
+        this.client
+          .login(process.env.discord_token)
+          .then(_ => console.log('Connected to Discord'))
+          .catch((error) =>
+            console.error(`Could not connect. Error: ${error.message}`)
+          );
+      }
+  
+
+    disconnect(): void {
+        this.client.destroy();
+    }
+
+    private initializeClient(): void{
+        if(!this.client) return;
+
+
+    }
+
+}
