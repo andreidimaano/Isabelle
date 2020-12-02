15761c1c7f3245642089977387ce0f3f8298b869
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-01 21:56:37 -0800

    add pomodoro tests

--- src/test/DiscordParse.test.ts
+++ src/test/DiscordParse.test.ts
@@ -8,7 +8,14 @@ let parseCommand = (messageContent: string): Arguments => {
     let args = content.split(' ').map((argument) => argument.trim());
     let parameter1: any;
     let parameter2: any;
-    if(args[0].toLowerCase() == 'league'){
+    
+    if(args[0].toLowerCase() == 'kanye') {
+        if(args.length > 1) {
+            return {
+                command: undefined
+            }
+        }
+    } else if(args[0].toLowerCase() == 'league') {
         if(args.length <= 1) {
             return {
                 command: undefined
@@ -20,9 +27,26 @@ let parseCommand = (messageContent: string): Arguments => {
                 if(x != args.length - 1) parameter1 += ' ';
             }
         }
+    } else if (args[0].toLowerCase() == 'pomodoro') {
+        //short short
+        if(args.length == 1) {
+            //default case
+            return {
+                command: args[0].toLowerCase(),
+                parameter1: 'short',
+                parameter2: 'short'
+            }
+        } else if (args.length == 2) {
+            return {
+                command: undefined
+            }
+        } else {
+            parameter1 = (args.length >= 2) ? args[1] : undefined;
+            parameter2 = (args.length >= 3) ? args[2] : undefined;
+        }
     } else {
-             parameter1 = (args.length >= 2) ? args[1] : undefined;
-             parameter2 = (args.length >= 3) ? args[2] : undefined;
+        parameter1 = (args.length >= 2) ? args[1] : undefined;
+        parameter2 = (args.length >= 3) ? args[2] : undefined; 
     }
     //(1) kanye
     //(2) league summoner Name
@@ -36,7 +60,7 @@ let parseCommand = (messageContent: string): Arguments => {
 }
 
 describe('ParseCommandTests', () => {
-    describe('League', () => {
+    describe('Kanye', () => {
         it('should return just kanye', () => {
             let expectedValue : Arguments = {
                 command: 'kanye'
@@ -44,8 +68,23 @@ describe('ParseCommandTests', () => {
             expect(parseCommand('~ kanye')).toEqual(expectedValue);
         });
     });
-    describe('two argument', () => {
-        it('should return correct league parse', () => {
+    describe('KanyeMultipleArguments', () => {
+        it('should return just kanye', () => {
+            let expectedValue : Arguments = {
+                command: undefined
+            };
+            expect(parseCommand('~ kanye west')).toEqual(expectedValue);
+        });
+    });
+    describe('League', () => {
+        it('should return undefined', () => {
+            let expectedValue : Arguments = {
+                command: undefined
+            };
+
+            expect(parseCommand('~ league')).toEqual(expectedValue);
+        });
+        it('should return El Platano', () => {
             let expectedValue : Arguments = {
                 command: 'league',
                 parameter1: 'El Platano'
@@ -53,13 +92,40 @@ describe('ParseCommandTests', () => {
 
             expect(parseCommand('~ league El Platano')).toEqual(expectedValue);
         });
-        it('should return just the command', () => {
+        it('should return Trieuloo', () => {
             let expectedValue : Arguments = {
                 command: 'league',
                 parameter1: 'Trieuloo'
             };
 
             expect(parseCommand('~ league Trieuloo')).toEqual(expectedValue);
+        });
+    });
+    describe('Pomdoro', () => {
+        it('should return default', () => {
+            let expectedValue : Arguments = {
+                command: 'pomodoro',
+                parameter1: 'short',
+                parameter2: 'short'
+            };
+
+            expect(parseCommand('~ pomodoro')).toEqual(expectedValue);
+        });
+        it('should return correct pomodoro', () => {
+            let expectedValue : Arguments = {
+                command: 'pomodoro',
+                parameter1: 'short',
+                parameter2: 'long'
+            };
+
+            expect(parseCommand('~ pomodoro short long')).toEqual(expectedValue);
+        });
+        it('should return undefine', () => {
+            let expectedValue : Arguments = {
+                command: undefined,
+            };
+
+            expect(parseCommand('~ pomodoro short')).toEqual(expectedValue);
         });
     });
 });
