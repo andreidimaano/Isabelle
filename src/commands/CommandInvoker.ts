98d5e143ff60b654caf53087f0bcd1d449933b66
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-01 22:04:24 -0800

    finish tests

--- src/commands/CommandInvoker.ts
+++ src/commands/CommandInvoker.ts
@@ -33,19 +33,62 @@ export class CommandInvoker {
     }
 
     executeCommand() : void {
-        this.command.execute();
+        if(this.command == undefined) {
+            console.log('invalid command');
+        } else {
+            this.command.execute();
+        }
     }
 
     private parseCommand(messageContent: string): Arguments {
         let content = messageContent.slice(this.prefix.length);
         let args = content.split(' ').map((argument) => argument.trim());
+        let parameter1: any;
+        let parameter2: any;
         
-        let parameter1 = (args.length >= 2) ? args[1] : undefined;
-        let parameter2 = (args.length >= 3) ? args[2] : undefined;
+        if(args[0].toLowerCase() == 'kanye') {
+            if(args.length > 1) {
+                return {
+                    command: undefined
+                }
+            }
+        } else if(args[0].toLowerCase() == 'league') {
+            if(args.length <= 1) {
+                return {
+                    command: undefined
+                }
+            } else {
+                parameter1 = '';
+                for(let x: number = 1; x < args.length; x++) {
+                    parameter1 += args[x];
+                    if(x != args.length - 1) parameter1 += ' ';
+                }
+            }
+        } else if (args[0].toLowerCase() == 'pomodoro') {
+            //short short
+            if(args.length == 1) {
+                //default case
+                return {
+                    command: args[0].toLowerCase(),
+                    parameter1: 'short',
+                    parameter2: 'short'
+                }
+            } else if (args.length == 2) {
+                return {
+                    command: undefined
+                }
+            } else {
+                parameter1 = (args.length >= 2) ? args[1] : undefined;
+                parameter2 = (args.length >= 3) ? args[2] : undefined;
+            }
+        } else {
+            parameter1 = (args.length >= 2) ? args[1] : undefined;
+            parameter2 = (args.length >= 3) ? args[2] : undefined; 
+        }
         //(1) kanye
         //(2) league summoner Name
         //(3) pomodoro short short
-
+    
         return {
             command: args[0].toLowerCase(),
             parameter1: parameter1,
