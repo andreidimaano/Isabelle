2272669a5c7f352d18168ba09176753b384ff83e
Samuel Lagunas <slagu002@ucr.edu>
2020-12-07 13:23:23 -0800

    more test cases

--- src/test/DiscordArray.test.ts
+++ src/test/DiscordArray.test.ts
@@ -88,5 +88,52 @@ describe('Discord Array test', () => {
         bot.addMemberStudying('Jarvis');
         expect(bot.getMembersStudying()).toHaveLength(2);
     })
+    it('should add to the array multiple', () => {
+        let bot = DiscordBot.getInstance();
+        bot.resetArray();
+        
+        expect(bot.getMembersOnBreak()).toHaveLength(0);
+        
+        bot.addMemberOnBreak('Andrei');
+        expect(bot.getMembersOnBreak()).toHaveLength(1);
+
+        bot.addMemberOnBreak('Sam');
+        expect(bot.getMembersOnBreak()).toHaveLength(2);
+        bot.removeMemberOnBreak('Sam');
+
+        bot.addMemberOnBreak('Jarvis');
+        expect(bot.getMembersOnBreak()).toHaveLength(2);
+    })
+    it('should remove', () => {
+        let bot = DiscordBot.getInstance();
+        bot.resetArray();
+        
+        bot.addMemberStudying('Andrei');
+        bot.addMemberStudying('Sam');
+        bot.addMemberStudying('Jarvis');
+        bot.addMemberStudying('Roger');
+        bot.addMemberStudying('Steve');
+        bot.addMemberStudying('Tony Stark');
+        expect(bot.getMembersStudying()).toHaveLength(6);
+
+        bot.removeMemberStudying('Andrei')
+        expect(bot.getMembersStudying()).toHaveLength(5);
+    })
+    it('should remove', () => {
+        let bot = DiscordBot.getInstance();
+        bot.resetArray();
+        
+        bot.addMemberOnBreak('Andrei');
+        bot.addMemberOnBreak('Sam');
+        bot.addMemberOnBreak('Jarvis');
+        bot.addMemberOnBreak('Roger');
+        bot.addMemberOnBreak('Steve');
+        bot.addMemberOnBreak('Tony Stark');
+        expect(bot.getMembersOnBreak()).toHaveLength(6);
+
+        bot.removeMemberOnBreak('Andrei')
+        expect(bot.getMembersOnBreak()).toHaveLength(5);
+
+    })
 
 })
