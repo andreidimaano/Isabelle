0fcdba6ed86d51107777bdc55f8b47a74108a0eb
Samuel Lagunas <slagu002@ucr.edu>
2020-12-06 20:49:02 -0800

    adding shortlong test

--- src/test/Discord.Pomodoro.test.ts
+++ src/test/Discord.Pomodoro.test.ts
@@ -20,6 +20,25 @@ let shortshort = () => {
             console.log('break over')
             //remove from break list
         }, 1000 * 5);
+
+    }, 1000 * 25); 
+}
+
+
+let shortlong = () => {
+    console.log('timer set to 25');
+    addMember();
+    setTimeout(async () => {
+        //remove from studying list 
+        removeMember();
+        addMember();    
+
+        console.log('break');
+        setTimeout(async () => {
+            removeMember();
+            console.log('break over')
+            //remove from break list
+        }, 1000 * 10);
         
     }, 1000 * 25); 
 }
