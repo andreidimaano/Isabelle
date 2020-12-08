5ee9ebfd6d63e019fd5d775d1d5599e4330de993
Jarvis Bui <jbui019@ucr.edu>
2020-12-07 20:12:49 -0800

    added unit test for same id test case

--- src/test/Discord.Riot.test.ts
+++ src/test/Discord.Riot.test.ts
@@ -36,9 +36,36 @@ describe('executeRiot', () => {
     
             let riot = getAccount('El Platano');
             riot.then(response => {
-                expect(response.name).toBe(data.name);
+                if(response.summonerId == undefined){
+                    expect(response).toBe(429)
+                }
+                else{
+                    expect(response.name).toBe(data.name);
+                }
             })  
         })
+
+        it('should be the same account', async () => {
+            let accountId = "4kGjw1InJwDgDBCazWqB3BwRXe1PfojVdAI1ApJjpgAjtDg";
+            let id = "O_1ySJMgZ_UQgjZtZ7hloLH85HsVoQZgmxV1iFvbK1HNNLk";
+            let name = "El Platano";
+    
+            let data = {
+                accountId: accountId,
+                id: id,
+                name: name
+            };
+    
+            let riot = getAccount('el platano');
+            riot.then(response => {
+                if(response.summonerId == undefined) {
+                    expect(response).toBe(429)
+                } else {
+                    expect(response.name).toBe(data.name);
+                }
+            })  
+        })
+        
         it('should be the same id', async () => {
             let accountId = "4kGjw1InJwDgDBCazWqB3BwRXe1PfojVdAI1ApJjpgAjtDg";
             let id = "O_1ySJMgZ_UQgjZtZ7hloLH85HsVoQZgmxV1iFvbK1HNNLk";
@@ -55,6 +82,7 @@ describe('executeRiot', () => {
                 expect(response.summonerId).toBe(data.id);
             }) 
         })
+
         it('should be the account id', async () => {
             let accountId = "4kGjw1InJwDgDBCazWqB3BwRXe1PfojVdAI1ApJjpgAjtDg";
             let id = "O_1ySJMgZ_UQgjZtZ7hloLH85HsVoQZgmxV1iFvbK1HNNLk";
@@ -71,6 +99,7 @@ describe('executeRiot', () => {
                 expect(response.accountId).toBe(data.accountId);
             })
         })
+
         it('should be the same name', async () => {
             let accountId = "3NHFJA9EQMFwDB4Q__7eD5wFS0DmCck_QUAiPgnh2Ucuvyw";
             let id = "IXHMPoOk4f57Qp7I-J2__yPkutc7m0c4u8VSpKR2tkvP_w";
@@ -87,11 +116,14 @@ describe('executeRiot', () => {
                 expect(response.name).toBe(data.name);
             }) 
         })
+
         it('should return error', async () => {
             let riot = getAccount('fawerfwefr');
             riot.then(response => {
                 expect(response).toBe(404);
             })
         })
+
+
     })
 })
