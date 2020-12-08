d7d9b1c2d1e3ae89835aa29451201452469fd960
Jarvis Bui <jbui019@ucr.edu>
2020-12-07 20:25:18 -0800

    Added test case with different user

--- src/test/Discord.Riot.test.ts
+++ src/test/Discord.Riot.test.ts
@@ -293,7 +293,28 @@ describe('executeRiot', () => {
             }) 
         })
 
-        
+        it('should be the same name', async () => {
+            let accountId = "3NHFJA9EQMFwDB4Q__7eD5wFS0DmCck_QUAiPgnh2Ucuvyw";
+            let id = "IXHMPoOk4f57Qp7I-J2__yPkutc7m0c4u8VSpKR2tkvP_w";
+            let name = "Thefate12";
+    
+            let data = {
+                accountId: accountId,
+                id: id,
+                name: name
+            };
+    
+            let riot = getAccount('The fate 12');
+            riot.then(response => {
+                if(response.summonerId == undefined) {
+                    expect(response).toBe(429)
+                } else {
+                    expect(response.name).toBe(data.name);
+                }
+            }) 
+        })
+
+
 
 
         it('should return error', async () => {
