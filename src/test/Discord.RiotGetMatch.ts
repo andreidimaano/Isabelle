e047c426613dd6b776ae64960a19cf5dc7b42e35
Jarvis Bui <jbui019@ucr.edu>
2020-12-07 21:02:41 -0800

    added first test case

--- src/test/Discord.RiotGetMatch.ts
+++ src/test/Discord.RiotGetMatch.ts
@@ -35,3 +35,28 @@ let getMatch = async (accountId: string, summonerId: string, amountOfGames: numb
         champId: champId
     }
 }
+
+describe('executeRiot', () =>{
+
+    describe('getMatch', () => {
+        it('should be the same match', async () => {
+            let data = {
+                gameId: 3695716766,
+                id: 'O_1ySJMgZ_UQgjZtZ7hloLH85HsVoQZgmxV1iFvbK1HNNLk',
+                champId: 81,
+            };
+    
+            let riot = getAccount('El Platano');
+            riot.then(response => {
+                if(response.gameId == undefined) {
+                    expect(response).toBe(429)
+                } else {
+                    expect(response.gameId).toBe(data.gameId);
+                    expect(response.champId).toBe(data.champId);
+                    expect(response.summonerId).toBe(data.id);
+                }
+            })
+        }) 
+    })
+    
+})
